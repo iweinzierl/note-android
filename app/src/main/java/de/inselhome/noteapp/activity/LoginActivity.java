88f3c8f5f32fc7e2dd3f73fd54f28c2c3d7af08b
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2015-05-19 12:39:33 +0200

    Improved the layout of the login screen.

--- app/src/main/java/de/inselhome/noteapp/activity/LoginActivity.java
+++ app/src/main/java/de/inselhome/noteapp/activity/LoginActivity.java
@@ -54,7 +54,7 @@ public class LoginActivity extends Activity {
         passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                if (id == EditorInfo.IME_NULL) {
                     attemptLogin();
                     return true;
                 }
